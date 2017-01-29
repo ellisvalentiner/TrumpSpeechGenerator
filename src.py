5c46df2cc35672d8b7e3a99d526c1ce18cd0a918
Ellis Valentiner <ellisvalentiner@gmail.com>
2017-01-29 14:11:53 -0500

    remove comment lines

--- src.py
+++ src.py
@@ -3,14 +3,11 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation
 from keras.layers import LSTM
 from keras.optimizers import RMSprop
-#from keras.utils.data_utils import get_file
 import numpy as np
 import random
 import sys
 import glob
-# path = get_file('nietzsche.txt', origin="https://s3.amazonaws.com/text-datasets/nietzsche.txt")
-# text = open(path).read().lower()
-# print('corpus length:', len(text))
+
 speeches = glob.glob("speeches/*.txt")
 text = ""
 for speech in speeches:
