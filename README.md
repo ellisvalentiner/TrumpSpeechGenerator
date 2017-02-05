80adc55e4c011da1f8f3ffabfef43c5b67f128c7
Ellis Valentiner <ellisvalentiner@gmail.com>
2017-02-05 13:39:24 -0500

    Pruning the repo

--- README.md
+++ README.md
@@ -6,6 +6,31 @@ The speeches are sourced from [hrbrmstr/orangetext](https://github.com/hrbrmstr/
 
 Since the text generating model is a LSTM, and RNNs are slow, it takes some time before the model begins to generate output that isn't complete gibberish.
 
+I suggest installing the requirements in a virtual environment:
+
 ```bash
-python src.py
+$ git clone git@github.com:ellisvalentiner/TrumpSpeechGenerator.git
+$ cd TrumpSpeechGenerator
+TrumpSpeechGnerator $ mkvirtualenv Keras
+(Keras) TrumpSpeechGnerator $ pip install -r requirements.txt
+(Keras) TrumpSpeechGenerator $ ipython src.py > log
 ```
+
+You then `tail -f log` to check in on the model:
+
+```python
+Using TensorFlow backend.
+corpus length: 163283
+total chars: 92
+nb sequences: 54408
+Vectorization...
+Build model...
+
+--------------------------------------------------
+Iteration 1
+Epoch 1/10
+54400/54408 [============================>.] - ETA: 0s - loss: 2.7732Epoch 00000: loss improved from inf to 2.77315, saving model to models/weights-00-2.7731.hdf5
+54408/54408 [==============================] - 1646s - loss: 2.7731
+```
+
+Notice it took 1646s to complete one epoch on my MacBook Pro (Retina, 15-inch, Mid 2014) with the 2.5 GHz Intel Core i7 processor.
--- src.py
+++ src.py
@@ -2,8 +2,8 @@ from __future__ import print_function
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
 from keras.layers import LSTM
-from keras.callbacks import ModelCheckpoint
-from keras.optimizers import RMSprop
+from keras.callbacks import ModelCheckpoint, History, EarlyStopping, ReduceLROnPlateau, TensorBoard
+from keras.utils.visualize_util import plot
 import numpy as np
 import random
 import sys
@@ -13,8 +13,7 @@ speeches = glob.glob("orangetext/data/speeches/*.txt")
 text = ""
 for speech in speeches:
     for line in open(speech, 'r'):
-        text += line
-text = " ".join(text.split())
+        text += " ".join(line.split())
 print('corpus length:', len(text))
 
 chars = sorted(list(set(text)))
@@ -41,23 +40,27 @@ for i, sentence in enumerate(sentences):
     y[i, char_indices[next_chars[i]]] = 1
 
 
-# build the model: a single LSTM
-print('Build model...')
+# build the model
+print('Building model...')
 model = Sequential()
-model.add(LSTM(256, input_shape=(maxlen, len(chars)), return_sequences=True))
+model.add(LSTM(128, input_shape=(maxlen, len(chars)), return_sequences=True))
 model.add(Dropout(0.2))
-model.add(LSTM(256))
+model.add(LSTM(128))
+model.add(Dropout(0.2))
+model.add(LSTM(128))
 model.add(Dropout(0.2))
 model.add(Dense(len(chars)))
 model.add(Activation('softmax'))
-
-#optimizer = RMSprop(lr=0.01)
-model.compile(loss='categorical_crossentropy', optimizer='adam')
+model.compile(loss='categorical_crossentropy', optimizer='RMSprop')
 
 # callbacks
-checkpoint = ModelCheckpoint('models/weights-{epoch:02d}-{loss:.4f}.hdf5',
-    monitor='loss', verbose=1, save_best_only=True, mode='min')
-callbacks_list = [checkpoint]
+checkpoint = ModelCheckpoint('models/weights-{epoch:02d}-{loss:.4f}.hdf5', monitor='loss', verbose=1,
+                             save_best_only=True, mode='min')
+history = History()
+early_stop = EarlyStopping(monitor='loss', min_delta=1e-4, patience=1, verbose=1, mode='min')
+reduce_lr = ReduceLROnPlateau(monitor='loss', factor=0.2, patience=1, min_lr=0.001, mode='min')
+tensor_board = TensorBoard(log_dir='./logs', histogram_freq=0, write_graph=True, write_images=True)
+callbacks_list = [checkpoint, history, early_stop, reduce_lr, tensor_board]
 
 def sample(preds, temperature=1.0):
     # helper function to sample an index from a probability array
@@ -69,11 +72,12 @@ def sample(preds, temperature=1.0):
     return np.argmax(probas)
 
 # train the model, output generated text after each iteration
-for iteration in range(1, 60):
+for iteration in range(1, 20):
     print()
     print('-' * 50)
     print('Iteration', iteration)
-    model.fit(X, y, batch_size=64, nb_epoch=10, callbacks=callbacks_list)
+    model.fit(X, y, batch_size=32, nb_epoch=1, callbacks=callbacks_list)
+    plot(model, to_file='models/model-{epoch:02d}-{loss:.4f}.png', show_shapes=True)
 
     start_index = random.randint(0, len(text) - maxlen - 1)
 
@@ -83,6 +87,32 @@ for iteration in range(1, 60):
 
         generated = ''
         sentence = text[start_index: start_index + maxlen]
+        generated += sentence
+        print('----- Generating with seed: "' + sentence + '"')
+        sys.stdout.write(generated)
+
+        for i in range(400):
+            x = np.zeros((1, maxlen, len(chars)))
+            for t, char in enumerate(sentence):
+                x[0, t, char_indices[char]] = 1.
+
+            preds = model.predict(x, verbose=0)[0]
+            next_index = sample(preds, diversity)
+            next_char = indices_char[next_index]
+
+            generated += next_char
+            sentence = sentence[1:] + next_char
+
+            sys.stdout.write(next_char)
+            sys.stdout.flush()
+        print()
+
+    for diversity in [0.1, 0.2, 0.5, 1.0, 1.2, 2.0]:
+        print()
+        print('----- diversity:', diversity)
+
+        generated = ''
+        sentence = "My fellow Americans, "
         generated += sentence
         print('----- Generating with seed: "' + sentence + '"')
         sys.stdout.write(generated)
