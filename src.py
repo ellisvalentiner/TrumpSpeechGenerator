8925c0f50148836079e5e06aa359c821a60c4f4f
Ellis Valentiner <ellisvalentiner@gmail.com>
2017-02-06 16:28:36 -0500

    Tweaks

--- src.py
+++ src.py
@@ -2,8 +2,8 @@ from __future__ import print_function
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
 from keras.layers import LSTM
+from keras.optimizers import RMSprop, Adam, Nadam
 from keras.callbacks import ModelCheckpoint, History, EarlyStopping, ReduceLROnPlateau, TensorBoard
-from keras.utils.visualize_util import plot
 import numpy as np
 import random
 import sys
@@ -43,19 +43,18 @@ for i, sentence in enumerate(sentences):
 # build the model
 print('Building model...')
 model = Sequential()
-model.add(LSTM(128, input_shape=(maxlen, len(chars)), return_sequences=True))
+model.add(LSTM(256, input_shape=(maxlen, len(chars)), return_sequences=True))
 model.add(Dropout(0.2))
-model.add(LSTM(128))
-model.add(Dropout(0.2))
-model.add(LSTM(128))
+model.add(LSTM(256))
 model.add(Dropout(0.2))
 model.add(Dense(len(chars)))
 model.add(Activation('softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='RMSprop')
+
+optimizer = RMSprop(lr=0.01, rho=0.9, epsilon=1e-08, decay=0.0)
+model.compile(loss='categorical_crossentropy', optimizer=optimizer)
 
 # callbacks
-checkpoint = ModelCheckpoint('models/weights-{epoch:02d}-{loss:.4f}.hdf5', monitor='loss', verbose=1,
-                             save_best_only=True, mode='min')
+checkpoint = ModelCheckpoint('models/weights-{epoch:02d}-{loss:.4f}.hdf5', monitor='loss', verbose=1, save_best_only=True, mode='min')
 history = History()
 early_stop = EarlyStopping(monitor='loss', min_delta=1e-4, patience=1, verbose=1, mode='min')
 reduce_lr = ReduceLROnPlateau(monitor='loss', factor=0.2, patience=1, min_lr=0.001, mode='min')
@@ -72,47 +71,20 @@ def sample(preds, temperature=1.0):
     return np.argmax(probas)
 
 # train the model, output generated text after each iteration
-for iteration in range(1, 20):
+for iteration in range(1, 21):
     print()
     print('-' * 50)
     print('Iteration', iteration)
-    model.fit(X, y, batch_size=32, nb_epoch=1, callbacks=callbacks_list)
-    plot(model, to_file='models/model-{epoch:02d}-{loss:.4f}.png', show_shapes=True)
+    model.fit(X, y, batch_size=64, nb_epoch=5, callbacks=callbacks_list)
 
     start_index = random.randint(0, len(text) - maxlen - 1)
 
-    for diversity in [0.1, 0.2, 0.5, 1.0, 1.2, 2.0]:
+    for diversity in [0.2, 0.5, 1.0, 1.2]:
         print()
         print('----- diversity:', diversity)
 
         generated = ''
         sentence = text[start_index: start_index + maxlen]
-        generated += sentence
-        print('----- Generating with seed: "' + sentence + '"')
-        sys.stdout.write(generated)
-
-        for i in range(400):
-            x = np.zeros((1, maxlen, len(chars)))
-            for t, char in enumerate(sentence):
-                x[0, t, char_indices[char]] = 1.
-
-            preds = model.predict(x, verbose=0)[0]
-            next_index = sample(preds, diversity)
-            next_char = indices_char[next_index]
-
-            generated += next_char
-            sentence = sentence[1:] + next_char
-
-            sys.stdout.write(next_char)
-            sys.stdout.flush()
-        print()
-
-    for diversity in [0.1, 0.2, 0.5, 1.0, 1.2, 2.0]:
-        print()
-        print('----- diversity:', diversity)
-
-        generated = ''
-        sentence = "My fellow Americans, "
         generated += sentence
         print('----- Generating with seed: "' + sentence + '"')
         sys.stdout.write(generated)
